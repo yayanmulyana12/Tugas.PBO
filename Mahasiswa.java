da3765541580fe6e8e28dd6ff0f5b3b1e69530ca
yayanmulyana12 <yayantotsuka@gmail.com>
2024-08-26 16:40:40 +0700

    Add files via upload

---
+++ Mahasiswa.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pkg23057_yayan.mulyana_praktikum_tugas1_pertemuan1;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Mahasiswa {
+    void membaca(){
+        System.out.println("Yayan sedang membaca buku di kelas");
+    }
+    
+    void nyontek(){
+        System.out.println("Yayan mencontek kepada temannya");
+        
+    }
+    
+    void modifikasi(){
+        System.out.println("Yayan memodifikasi codingannya");
+    }
+    
+}
---
+++ MahasiswaBeraksi.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg23057_yayan.mulyana_praktikum_tugas1_pertemuan1;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class MahasiswaBeraksi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Mahasiswa mahasiswaunsika = new Mahasiswa();
+        
+        mahasiswaunsika.membaca();
+        mahasiswaunsika.nyontek();
+        mahasiswaunsika.modifikasi();
+                
+        // TODO code application logic here
+    }
+    
+}
